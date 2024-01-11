58a4aa1dc98b7bffd937052c5feed7db238cb945
andryxatt <vlasandrey878@gmail.com>
2024-01-11 11:36:11 +0200

    add cors policy

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,19 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { ValidationPipe } from "@nestjs/common";
 import * as express from "express";
 import { join } from "path";
+import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.interface";
 async function bootstrap() {
+  const corsOptions: CorsOptions = {
+    origin: "http://step-in-style.shop", // Replace with your React app's domain
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    optionsSuccessStatus: 204,
+  };
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder().setTitle("Web Api").setDescription("The webapi API description").setVersion("1.0").addBearerAuth().addTag("api").build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
-  app.enableCors();
+  app.enableCors(corsOptions);
   app.use("/uploads/files/", express.static(join(__dirname, "..", "uploads/files/")));
   app.useGlobalPipes(new ValidationPipe());
   app.listen(parseInt(process.env.PORT) || 3000);
