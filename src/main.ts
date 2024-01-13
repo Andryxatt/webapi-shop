745efd4c39f7a6c59475bca97005d52a992bd894
andryxatt <vlasandrey878@gmail.com>
2024-01-13 22:18:08 +0200

    cors error

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,7 @@ import * as express from "express";
 import { join } from "path";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  });
+  app.enableCors();
   const config = new DocumentBuilder().setTitle("Web Api").setDescription("The webapi API description").setVersion("1.0").addBearerAuth().addTag("api").build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
