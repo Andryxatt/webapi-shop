68b20b3b9d53b5018606cefc86d11e85844110b1
andryxatt <vlandroid228@gmail.com>
2024-02-06 18:24:42 +0200

    add changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { InjectDataSource, TypeOrmModule } from '@nestjs/typeorm';
 import { BrandsModule } from './brands/brands.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService  } from '@nestjs/config';
 import { DataSource } from 'typeorm';
 import { CategoriesModule } from './categories/categories.module';
 import { SubCategoriesModule } from './sub-categories/sub-categories.module';
@@ -28,17 +28,19 @@ import { ProductFeaturesModule } from './product-features/product-features.modul
       isGlobal: true,
     }),
     TypeOrmModule.forRootAsync({
-      useFactory: () => ({
+      imports:[ConfigModule],
+      useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: process.env.DB_HOST,
-        port: parseInt(process.env.DB_PORT) || 5432,
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD as string,
-        database: process.env.DB_DATABASE,
+        host:  configService.get('DB_HOST'),
+        port: +configService.get<number>('DB_PORT') || 5432,
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
         synchronize: true,
         autoLoadEntities: true,
-      })
+      }),
+      inject: [ConfigService]
     }),
     BrandsModule,
     CategoriesModule,
