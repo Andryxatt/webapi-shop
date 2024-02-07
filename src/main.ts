ed3196212f1b2d58e16affe5d300988d9fd516f4
andryxatt <vlandroid228@gmail.com>
2024-02-07 13:18:30 +0200

    bug host

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,7 @@ import * as dotenv from "dotenv";
 import { join } from "path";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const environment = process.env.NODE_ENV || "development";
-  const envFileName =".env"
-  dotenv.config({ path: envFileName });
+  dotenv.config();
   app.enableCors();
   const config = new DocumentBuilder().setTitle("Web Api").setDescription("The webapi API description").setVersion("1.0").addBearerAuth().addTag("api").build();
   const document = SwaggerModule.createDocument(app, config);
